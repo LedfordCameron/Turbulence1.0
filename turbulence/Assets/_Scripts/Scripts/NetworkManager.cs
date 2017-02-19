e0a9d6c24d7acada3fb103a21388ae9863cfb0e5
Cameron Ledford <Ledford_Cameron@columbusstate.edu>
2017-02-19 14:29:37 -0500

    Second commit

--- turbulence/Assets/_Scripts/Scripts/NetworkManager.cs
+++ turbulence/Assets/_Scripts/Scripts/NetworkManager.cs
@@ -6,11 +6,40 @@ public class NetworkManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		Connect ();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Connect(){
+		Debug.Log ("Connect");
+		PhotonNetwork.ConnectUsingSettings ("prototype v1.0");
+		//PhotonNetwork.ConnectToMaster("localhost", 13306, "5eaa723-97d8-4adc-955f-4547bc8ee4fc", "v1");
+		//PhotonNetwork.offlineMode = true;
 	}
+
+	//get rid of this method
+	void OnGUI(){
+		Debug.Log ("OnGUI");
+		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+	void OnJoinedLobby(){
+		Debug.Log ("OnJoinedLobby");
+		PhotonNetwork.JoinRandomRoom ();
+	}
+
+	void OnPhotonRandomJoinFailed(){
+		Debug.Log ("OnPhotonRandomJoinFailed");
+		PhotonNetwork.CreateRoom (null);
+	}
+
+	void OnJoinedRoom(){
+		Debug.Log ("OnJoinedRoom");
+		SpawnMyPlayer ();
+	}
+
+	void SpawnMyPlayer(){
+		Debug.Log ("SpawnMyPlayer");
+		PhotonNetwork.Instantiate ("Airplane", Vector3.zero, Quaternion.identity, 0);
+	}
+
 }
